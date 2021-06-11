655f4c490144afcad78fcc5e1643e8918831c6b4
cgerasimovitch <c.gerasimovitch@gmail.com>
2021-06-11 19:40:19 +0300

    NavigationBar.isHidden

---
+++ Navigation/Profile/LogInViewController.swift
@@ -0,0 +1,32 @@
+//
+//  LogInViewController.swift
+//  Navigation
+//
+//  Created by Admin on 6/11/21.
+//  Copyright © 2021 Artem Novichkov. All rights reserved.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .cyan
+        navigationController?.navigationBar.isHidden = true
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
