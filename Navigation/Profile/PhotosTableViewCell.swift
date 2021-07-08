541b22cff80f6caa9bd1473fd6fb5711e4592d6f
cheslav <cheslavg@gmail.com>
2021-07-08 20:50:12 +0300

    Made tableCell constaints

--- Navigation/Profile/PhotosTableViewCell.swift
+++ Navigation/Profile/PhotosTableViewCell.swift
@@ -9,16 +9,40 @@
 import UIKit
 
 class PhotosTableViewCell: UITableViewCell {
+    //Сделайте верстку согласно макету. В ячейке должно отображаться первые 4 фото.
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
+
+    let userImageView = UIImageView()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+            super.init(style: style, reuseIdentifier: reuseIdentifier)
+    
         
+
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupUserImage(image: UIImageView) {
+        self.addSubview(image)
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.clipsToBounds = true
+        image.layer.cornerRadius = 6
+        let window = UIWindow()
+        let borderOffset:CGFloat = 12
+        let inlineOffset:CGFloat = 8
+        
+        let imageWidth = (window.frame.width - borderOffset*2 - inlineOffset*2)/4
+        
+        NSLayoutConstraint.activate([
+            image.widthAnchor.constraint(equalToConstant: imageWidth),
+            image.heightAnchor.constraint(equalTo: image.widthAnchor),
+            image.topAnchor.constraint(equalTo: self.topAnchor, constant: 12),
+            image.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: 12)
+        ])
+        
     }
 
 }
