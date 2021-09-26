77a9257a80ba20a3e47c6933ef59bdf683d679ea
cgerasimovitch <c.gerasimovitch@gmail.com>
2021-09-26 22:50:14 +0300

    Finish fixing merge

--- Navigation/PostViewController.swift
+++ Navigation/PostViewController.swift
@@ -11,10 +11,4 @@ import UIKit
 class PostViewController: UIViewController {
     
     var post: Post?
-
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        //title = post?.title
-    }
 }
